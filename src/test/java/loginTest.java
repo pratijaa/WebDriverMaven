add0e39e7ed8c66dbb9c671a5a3398a1b2ba495f
pratija <ppratija@gmail.com>
2017-12-12 11:12:00 +0530

    initial commit

---
+++ src/test/java/loginTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class loginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setup()
+	{
+		
+	System.setProperty("webdriver.chrome.driver", "D:\\class\\webdriver\\chromedriver_win32\\chromedriver.exe");
+
+	driver=new ChromeDriver();
+	
+}
+	@Test
+	public void doLogin() {
+	
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("pratijapavitwar");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("12345");
+		//driver.findElement(By.xpath(".//*[@id='loginbutton']")).click();
+	}
+	
+	@AfterTest 
+	public void tearDown() {
+		driver.quit();
+	}
+}
